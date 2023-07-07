1072752147e64c1f33d0cef97586af99a4dbc695
Argel <argelhr@hotmail.com>
2023-07-07 17:28:01 -0300

    conexao funcionando

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import paramiko
 
 from paho.mqtt import client as mqtt_client
 
-broker = 'ubuntu@tsi1.duckdns.org'
+broker = 'tsi1.duckdns.org'
 port = 1883
 topic = "sensor1"
 # generate client ID with pub prefix randomly
@@ -15,12 +15,6 @@ client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
 # username = 'emqx'
 # password = 'public'
-
-# Configuração SSH
-#ssh_host = 'ubuntu@tsi1.duckdns.org'
-#ssh_port = 22
-#ssh_username = 'argel'
-#ssh_private_key_path = ""
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -45,17 +39,13 @@ def publish(client):
         # result: [0, 1]
         status = result[0]
         if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
+            print(f"`{client_id}` - Send `{msg}` to topic `{topic}`")
         else:
             print(f"Failed to send message to topic {topic}")
         msg_count += 1
 
 
 def run():
-    #Conexão SSH
-    #ssh = paramiko.SSHClient()
-    #ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #ssh.connect(ssh_host, ssh_port, ssh_username, key_filename=ssh_private_key_path)
 
     # Conexão MQTT
     client = connect_mqtt()
